1d65d039dbd3ccb507b6cea524ccdfcdadb1cfd5
Victor Sandoval <distraido_99@hotmail.com>
2022-12-08 12:20:57 -0600

    http request

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-//   res.status = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World");
-// });
-
-// server.listen(3000, () => {
-//   console.log("Server on port 3000");
-// });
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello word");
+app.get("/user", (req, res) => {
+  res.json({
+    username: "Victor",
+    lastname: "Sandoval",
+  });
+});
+
+app.post("/abaout", (req, res) => {
+  res.send("POST REQUEST RECEIVED");
+});
+
+app.put("/contact", (req, res) => {
+  res.send("UPDATE REQUEST RECEIVED");
+});
+
+app.delete("/test", (req, res) => {
+  res.send("<h1>DELETE REQUEST RECEIVED</h1>");
 });
 
 app.listen(3000, () => {
