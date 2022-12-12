0e5939c5925396282c3437e92b4328b17312f630
Victor Sandoval <distraido_99@hotmail.com>
2022-12-12 11:33:02 -0600

    Motores de plantilla ejs

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+//Settings
+app.set("appName", "Fazt Express Tutorial");
+app.set("port", "3000");
+app.set("view engine", "ejs");
+
+//Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 
+//Routes
 //simepre pasa por esa ruta en caso de poner next hasta ahi termina con next ejecuta las demas rutas
 // app.all("/user", (req, res, next) => {
 //   console.log("por aqui paso");
 //   //   res.send("finish");
 //   next();
 // });
+
+app.get("/", (req, res) => {
+  const data = [
+    { name: "Victor" },
+    { name: "Manuel" },
+    { name: "Daniel" },
+    { name: "Rayan" },
+  ];
+  res.render("index.ejs", { people: data });
+});
 
 app.get("/user", (req, res) => {
   res.json({
@@ -36,6 +53,7 @@ app.delete("/user/:userId", (req, res) => {
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log("Server on port 3000");
+app.listen(app.get("port"), () => {
+  console.log(app.get("appName"));
+  console.log("Server on port", app.get("port"));
 });
